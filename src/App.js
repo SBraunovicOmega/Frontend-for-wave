de8c676f5cb1340107e06d66770780c697168d91
Stefan <stefan.braunovic@vegait.rs>
2022-07-27 15:00:54 +0200

    view all posts after wallet is connected

--- src/App.js
+++ src/App.js
@@ -103,6 +103,7 @@ export default function App() {
       const accounts = await ethereum.request({ method: "eth_requestAccounts" });
 
       console.log("Connected", accounts[0]);
+      getAllWaves()
       setCurrentAccount(accounts[0]);
     } catch (error) {
       console.log(error)
